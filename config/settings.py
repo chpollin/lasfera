d681d303077f93b9ab7b284f1acada85c0d2d860
Jason Heppler <jason.heppler@gmail.com>
2024-02-20 14:42:06 -0600

    Convert from sqlite to postgres

--- config/settings.py
+++ config/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": env("DB_HOST", default="localhost"),
+        "PORT": env("DB_PORT", default="5432"),
+        "NAME": env("DB_NAME", default="lasfera"),
+        "USER": env("DB_USER", default="lasfera"),
+        "PASSWORD": env("DB_PASSWORD"),
     }
 }
 
