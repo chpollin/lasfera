a211ff401de01c6135e0bd063a92d63d1ca07dca
Jason Heppler <jason.heppler@gmail.com>
2024-04-22 16:13:56 -0500

    feat: Testing nested-admin

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", include("manuscript.urls")),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
+    path("_nested_admin/", include("nested_admin.urls")),
     path("prose/", include("prose.urls")),
 ]
