62c771bbdcb90046cb96a9573ddb7a4045650e3b
Jason Heppler <jason.heppler@gmail.com>
2024-05-31 12:55:25 -0500

    refactor: Update index view to include manuscripts
    This commit updates the index view in the `views.py` file to include the `SingleManuscript` model and pass it to the template. This change allows the template to display the manuscripts along with the stanzas.

--- manuscript/views.py
+++ manuscript/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
 
-from manuscript.models import Stanza
+from manuscript.models import SingleManuscript, Stanza
 
 
 def index(request: HttpRequest):
     stanzas = Stanza.objects.all().order_by("stanza_line_code_starts")
-    return render(request, "index.html", {"stanzas": stanzas})
+    manuscripts = SingleManuscript.objects.all()
+    return render(
+        request, "index.html", {"stanzas": stanzas, "manuscripts": manuscripts}
+    )
