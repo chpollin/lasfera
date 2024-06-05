e3d0b23848927e54d1ac775ba6595e9bb833e045
Jason Heppler <jason.heppler@gmail.com>
2024-06-05 13:23:22 -0500

    chore: Add login and logout views to url config

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path("", include("manuscript.urls")),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("prose/", include("prose.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
