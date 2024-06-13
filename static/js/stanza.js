ced34b9744cdd4e19ef17037fc9606a273d9ab50
Jason Heppler <jason.heppler@gmail.com>
2024-06-13 14:01:16 -0500

    refactor(design): Handle toggling of line codes and display of English translation

--- static/js/stanza.js
+++ static/js/stanza.js
@@ -23,3 +23,66 @@ closeButton.addEventListener("click", function () {
   // Hide the sidebar
   sidebar.style.transform = "translateX(100%)";
 });
+
+// Handle toggling line codes
+document.addEventListener("DOMContentLoaded", (event) => {
+  const toggleLineCodes = document.getElementById("toggleLineCodes");
+
+  if (toggleLineCodes) {
+    toggleLineCodes.addEventListener("click", function () {
+      console.log("line codes toggled");
+      var lineCodes = document.querySelectorAll(".line-code");
+      for (var i = 0; i < lineCodes.length; i++) {
+        if (this.checked) {
+          lineCodes[i].style.display = "inline";
+        } else {
+          lineCodes[i].style.display = "none";
+        }
+      }
+    });
+  }
+});
+
+// Handle shortening the line codes
+// We shorten the code from 01.01.01 to 1 (that is, the final .01)
+document.addEventListener("DOMContentLoaded", (event) => {
+  const shortenLineCodes = document.getElementById("toggleLineCodeShortened");
+
+  if (shortenLineCodes) {
+    let originalLineCodes = [];
+
+    shortenLineCodes.addEventListener("click", function () {
+      const lineCodes = document.querySelectorAll(".line-code");
+      for (let i = 0; i < lineCodes.length; i++) {
+        if (this.checked) {
+          originalLineCodes[i] = lineCodes[i].textContent;
+
+          let code = lineCodes[i].textContent;
+          let parts = code.split(".");
+          let lastPart = parts[parts.length - 1];
+          lineCodes[i].textContent = parseInt(lastPart);
+        } else {
+          if (originalLineCodes.length) {
+            lineCodes[i].textContent = originalLineCodes[i];
+          }
+        }
+      }
+    });
+  }
+});
+
+// Return to top button
+window.addEventListener("scroll", function () {
+  var returnToTop = document.getElementById("return-to-top");
+  var top = document.getElementById("top");
+  var distanceFromTop = top.getBoundingClientRect().top;
+
+  // Show the button after the #top anchor is 100px above the viewport
+  if (distanceFromTop < -100) {
+    returnToTop.classList.remove("opacity-0", "invisible");
+    returnToTop.classList.add("opacity-100");
+  } else {
+    returnToTop.classList.remove("opacity-100");
+    returnToTop.classList.add("opacity-0", "invisible");
+  }
+});
